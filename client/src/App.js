b06c41ac4669e3ec765c7308c4797347c2ad72fb
GeorgiosP <g.psarakis3800@gmail.com>
2017-10-17 23:33:10 -0400

    column and row data fetched from original get request will now fetch the actual form data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  state = {data: {}}
+  state = {
+    sheet: {},
+    rowCount: "",
+    columnCount: ""
+  };
 
-  componentWillMount(){
-    this.getData() 
-    console.log(this.state.data)
+  componentWillMount() {
+    this.getData();
   }
-  getData = () =>
-  {
-    const SPREADSHEET_ID = '1YXYqO1KTEPImoREg69Ln6vh1uI_jg83FmKQ1puulUOc'
-    const API_URL = 'https://sheets.googleapis.com/v4/spreadsheets'
-    const API_KEY = 'AIzaSyC2qA2hiYKbgdClhoiznyT0aI-F1TDTeMM'
-    fetch(API_URL+"/"+SPREADSHEET_ID+"/"+"?key="+API_KEY)
-    .then(res => res.json())
-    .then(data => console.log(data.sheets))
-
+  getData = () => {
+    const SPREADSHEET_ID = "1YXYqO1KTEPImoREg69Ln6vh1uI_jg83FmKQ1puulUOc";
+    const API_URL = "https://sheets.googleapis.com/v4/spreadsheets";
+    const API_KEY = "AIzaSyC2qA2hiYKbgdClhoiznyT0aI-F1TDTeMM";
+    fetch(API_URL + "/" + SPREADSHEET_ID + "/" + "?key=" + API_KEY)
+      .then(res => res.json())
+      .then(data => {
+        return this.setState({ sheet: { data } });
+      });
+  };
+  componentDidUpdate() {
+    const data = this.state.sheet.data;
+    let rowsAndColumn = {}
+    data.sheets.map(sheet =>{
+      rowsAndColumn = sheet.properties.gridProperties
+    }
+   
+    )
+    console.log(rowsAndColumn)
   }
-
   render() {
     return (
       <div className="App">
@@ -29,7 +41,6 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          
         </p>
       </div>
     );
