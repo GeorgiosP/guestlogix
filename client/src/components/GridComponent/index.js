d7f7da36c63534a7fe76bdb8f9f4baf192a8dd97
GeorgiosP <g.psarakis3800@gmail.com>
2017-10-23 14:37:57 -0400

    fixed spelling error on const to be dataTitle

--- client/src/components/GridComponent/index.js
+++ client/src/components/GridComponent/index.js
@@ -3,8 +3,8 @@ import {GridWrapper,LoadingIcon,LoadingWrapper} from './styles';
 import loadingIcon from '../../assets/images/loading.gif'
 
 class GridComponent extends Component {
-addDataBack = (dataTilte) =>{
-    this.props.data != undefined ? this.props.data.values.unshift(dataTilte) : null
+addDataBack = (dataTitle) =>{
+    this.props.data != undefined ? this.props.data.values.unshift(dataTitle) : null
 }
   render() {
 
@@ -12,20 +12,20 @@ addDataBack = (dataTilte) =>{
 //        setTimeout(() => this.setState({loading:false}),3500)
 //    return (<LoadingWrapper><LoadingIcon src={loadingIcon}/></LoadingWrapper>);
 //    }
-    const dataTilte =  this.props.data != undefined ? this.props.data.values.shift() : null
-    console.log(dataTilte)
+    const dataTitle =  this.props.data != undefined ? this.props.data.values.shift() : null
+    console.log(dataTitle)
     return(
     <GridWrapper>
         
         <table className="table is-fullwidth is-striped">
             <thead>
                 <tr>
-                { this.props.data != undefined ?  dataTilte.map((value,index) => <th key={index}>{value}</th>) : <th>Loading</th>}    
+                { this.props.data != undefined ?  dataTitle.map((value,index) => <th key={index}>{value}</th>) : <th>Loading</th>}    
                 </tr>
             </thead>
             <tbody>  
                {this.props.data != undefined ? this.props.data.values.map((row, index) => <tr key={index}>{row.map((column,index)=><td key={index}>{column}</td>)}</tr>) : <tr><td>Loading</td></tr>}
-                {this.addDataBack(dataTilte)}
+                {this.addDataBack(dataTitle)}
             </tbody>
         </table>
     </GridWrapper>
